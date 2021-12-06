67ee59c5a0e26140d604da032c1ae83f98e9b3f2
Daniel Houle <daniel.m.houle@gmail.com>
2021-12-05 21:54:19 -0600

    make day 5 part 1 run faster

--- src/aoc/2021/day5.clj
+++ src/aoc/2021/day5.clj
@@ -15,24 +15,16 @@
 
 (def input (slurp "resources/aoc/2021/day5.txt"))
 
-(defn build-matrix [x y]
-  (apply merge (for [i (range (inc x))
-                     j (range (inc y))]
-                 {[i j] 0})))
+(defn build-matrix [width height]
+  (make-array Long/TYPE (inc width) (inc height)))
+
 
 (defn update-matrix [matrix line]
-  (loop [line line
-         matrix matrix]
-    (let [point (first line)]
-      (if (not point) matrix
-          (let [point-on-map (get matrix point)]
-            (do (println point point-on-map)
-                (if (not point-on-map)
-                  (recur (rest line) matrix)
-                  (recur (rest line) (assoc matrix point (inc point-on-map)))
-                  ))))
-      ))
+  (for [[x y] line
+        :let [current-value (aget matrix x y)]]
+    (do (aset matrix x y (inc current-value))))
   )
+
 
 
 ;; [0 0] [0 5]
@@ -67,21 +59,19 @@
   ([input]
    (let [lines (str/split-lines input)
          coord-pairs-str (mapv (fn [line] (str/split line #" -> ")) lines)
-         coord-pairs (mapv (fn [pair] (mapv (fn [p] (mapv #(Integer/parseInt %) (str/split p #","))) pair)) coord-pairs-str)
+         coord-pairs (filterv (fn [[[x1 y1] [x2 y2]]] (or (= x1 x2) (= y1 y2))) (mapv (fn [pair] (mapv (fn [p] (mapv #(Integer/parseInt %) (str/split p #","))) pair)) coord-pairs-str))
          max-x (apply max (for [[[x]] coord-pairs] x))
          max-y (apply max (for [[[_ y]] coord-pairs] y))
          matrix (build-matrix max-x max-y)]
-     (loop [coord-pairs coord-pairs
-            matrix matrix]
-       (let [coord-pair (first coord-pairs)]
-         (if (not coord-pair)
-           (count (filter #(>= % 2) (vals matrix)))
-           (let [[[x1 y1] [x2 y2]] coord-pair]
-             (if (or (= x1 x2) (= y1 y2))
-               (let [line (build-line coord-pair)]
-                 (recur (rest coord-pairs) (update-matrix matrix line)))
-               (recur (rest coord-pairs) matrix)))))
-       ))
+     (doseq [coord-pair coord-pairs
+             [x y] (build-line coord-pair)
+             :let [val (aget matrix x y)]]
+       (aset matrix x y (inc val)))
+     (count (for [r (seq matrix)
+                  c (seq r)
+                  :when (>= c 2)]
+              c))
+     )
    ))
 
 
