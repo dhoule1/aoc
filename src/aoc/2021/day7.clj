d9287ee71a8915abdbc893a2c09748c8800eef6a
Daniel Houle <daniel.m.houle@gmail.com>
2021-12-07 20:27:28 -0600

    2021 day 7 part 2

--- src/aoc/2021/day7.clj
+++ src/aoc/2021/day7.clj
@@ -21,10 +21,36 @@
         min_pos (reduce min input)
         max_pos (reduce max input)]
     (second
-     (reduce (fn [[_ v1 :as m1] [_ v2 :as m2]] (do (println v1 v2) (if (> v1 v2) m2 m1)))
+     (reduce (fn [[_ v1 :as m1] [_ v2 :as m2]] (if (> v1 v2) m2 m1))
              (for [destination (range min_pos (inc max_pos))]
                [destination (reduce + (map (fn [i] (Math/abs (- i destination))) input))])))))
 
 (comment
   (part1 example)
   (part1 input))
+
+
+
+;;;;;;;;;;;;
+;; Part 2 ;;
+;;;;;;;;;;;;
+
+
+(defn part2 [input_raw]
+  (defn calculate-fuel-cost [origin destination]
+    (Math/abs (- origin destination)))
+
+  (let [input (parse input_raw)
+        min_pos (reduce min input)
+        max_pos (reduce max input)]
+    (second
+     (reduce (fn [[_ v1 :as m1] [_ v2 :as m2]] (if (> v1 v2) m2 m1))
+             (for [destination (range min_pos (inc max_pos))]
+               [destination (reduce + (map (fn [i]
+                                             (let [steps (Math/abs (- i destination))]
+                                               (+ steps (reduce + (range steps))))) input))])))))
+
+
+(comment
+  (part2 example)
+  (part2 input))
