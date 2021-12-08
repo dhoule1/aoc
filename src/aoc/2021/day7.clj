83345fee530f57d4b03709f234f10dd2ac4467e3
Daniel Houle <daniel.m.houle@gmail.com>
2021-12-07 20:18:02 -0600

    2021 day 7 part 1

---
+++ src/aoc/2021/day7.clj
@@ -0,0 +1,30 @@
+(ns aoc.2021.day7
+  (:require [clojure.string :as str]))
+
+(def example "16,1,2,0,4,2,7,1,2,14")
+
+(defn parse [input]
+  (-> input
+      (str/split #",")
+      (#(mapv (fn[x] (Integer/parseInt (str/trim x))) %))))
+
+(def input (slurp "resources/aoc/2021/day7.txt"))
+
+(parse example)
+
+
+(defn part1 [input_raw]
+  (defn calculate-fuel-cost [origin destination]
+    (Math/abs (- origin destination)))
+
+  (let [input (parse input_raw)
+        min_pos (reduce min input)
+        max_pos (reduce max input)]
+    (second
+     (reduce (fn [[_ v1 :as m1] [_ v2 :as m2]] (do (println v1 v2) (if (> v1 v2) m2 m1)))
+             (for [destination (range min_pos (inc max_pos))]
+               [destination (reduce + (map (fn [i] (Math/abs (- i destination))) input))])))))
+
+(comment
+  (part1 example)
+  (part1 input))
