94c057bbbb7204677fc4e201ba0e9ca727dff751
Daniel Houle <daniel.m.houle@gmail.com>
2021-12-08 20:52:19 -0600

    2021 day 8 part 1

---
+++ src/aoc/2021/day8.clj
@@ -0,0 +1,31 @@
+(ns aoc.2021.day8
+  (:require [clojure.string :as str]))
+
+
+(def example "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe
+edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc
+fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg
+fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb
+aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea
+fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb
+dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe
+bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef
+egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb
+gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce")
+
+
+(defn part1 [input]
+  (->> input
+       (str/split-lines)
+       (map (fn [line] (second (first (re-seq #"\| ([a-g ]+)" line)))))
+       (mapcat #(re-seq #"[a-g]+" %))
+       (filter #(contains? #{2 3 4 7} (count %)))
+       count))
+
+(part1 example)
+
+
+(def input (slurp "resources/aoc/2021/day8.txt"))
+
+
+(part1 input)
