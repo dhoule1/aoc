f0cdc2455fded1e7e1e607f67009c9b7ca569276
Daniel Houle <daniel.m.houle@gmail.com>
2021-12-09 21:56:04 -0600

    2021 day 9 part 2

--- src/aoc/2021/day9.clj
+++ src/aoc/2021/day9.clj
@@ -58,3 +58,52 @@
 
 (part1 example)
 (part1 input)
+
+
+
+;;;;;;;;;;;;
+;; Part 2 ;;
+;;;;;;;;;;;;
+
+
+
+
+(defn part2 [input]
+  (defn adj [matrix [x y]]
+    (for [[y' r] (map-indexed (fn [i e] [i e]) matrix)
+          :when (or (= y' y)
+                    (= y' (dec y))
+                    (= y' (inc y)))
+          [x' c] (map-indexed (fn [i e] [i e]) r)
+          :when (or (= x' x)
+                    (= x' (dec x))
+                    (= x' (inc x)))
+          :when (and (not= [x' y'] [x y])
+                     (or (= x' x) (= y' y)))]
+      {[x' y'] c}) )
+
+
+  (let [matrix (parse input)
+        basins
+        (for [[y r] (map-indexed (fn [i e] [i e]) matrix)
+              [x c] (map-indexed (fn [i e] [i e]) r)
+              :let [adj-cells (adj matrix [x y])]
+              :when (= (count adj-cells) (count (filter (fn [i] (> (first (vals i)) c)) adj-cells)))]
+          (loop [basin (filter (fn [x] (not= 9 (first (vals x)))) adj-cells)
+                 collected #{{[x y] c}}]
+            (if (empty? basin) collected
+                (let [b (first basin)
+                      addtl-basin (filter (fn [ab] (and (> (first (vals ab))
+                                                           (first (vals b)))
+                                                        (not= 9 (first (vals ab)))) )
+                                          (adj matrix (first (keys b)))) _ ]
+                  (recur (concat (rest basin) addtl-basin) (conj collected b))))))]
+    (reduce (fn [acc b1] (* acc (count b1))) 1
+            (take 3 (sort (fn [b1 b2] (> (count b1) (count b2))) basins)))
+    ))
+
+(count (nth
+        (part2 example) 3))
+
+(part2 example)
+(part2 input)
